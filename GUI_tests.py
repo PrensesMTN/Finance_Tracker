8d0f33f7ab8eaad4a26412779bde28c295f07d6f
Prenses Metin <60544998+PrensesMTN@users.noreply.github.com>
2024-03-02 01:40:38 +0300

    Update GUI_tests.py

--- GUI_tests.py
+++ GUI_tests.py
@@ -5,6 +5,9 @@ import tkinter as tk
 from tkinter import messagebox
 import csv
 import os
+from matplotlib import pyplot as plt
+import numpy as np
+
 
 customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -41,11 +44,11 @@ class App(customtkinter.CTk):
         self.sidebar_Yenile.grid(row=5, column=0, padx=20, pady=10) 
 
         # create main entry and button
-#         self.entry_ara = customtkinter.CTkEntry(self, placeholder_text="Aramak istediğiniz ürünü giriniz.")
-#         self.entry_ara.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
+        self.entry_ara = customtkinter.CTkEntry(self, placeholder_text="Aramak istediğiniz ürünü giriniz.")
+        self.entry_ara.grid(row=2, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
 
-#         self.main_button_ara = customtkinter.CTkButton(master=self,text="ARA", fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"),command=self.open_input_dialog_event)
-#         self.main_button_ara.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
+        self.main_button_ara = customtkinter.CTkButton(master=self,text="ARA", fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"),command=self.open_input_dialog_event)
+        self.main_button_ara.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
 # #
         # create treeview
         # self.vsb = customtkinter.CTkButton(master=self,text="ARA", fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"),command=self.open_input_dialog_event)
@@ -65,12 +68,65 @@ class App(customtkinter.CTk):
           #create main entry and button
           self.entry_ara = customtkinter.CTkEntry(self, placeholder_text="Aramak istediğiniz ürünü giriniz.")
           self.entry_ara.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
+          income()
 
           
  
         
 def income ():
         print("income")
+
+        # Creating dataset
+        size = 6
+        cars = ['AUDI', 'BMW', 'FORD',
+                        'TESLA', 'JAGUAR', 'MERCEDES']
+
+        data = np.array([[23, 16], [17, 23],
+                                        [35, 11], [29, 33],
+                                        [12, 27], [41, 42]])
+
+        # normalizing data to 2 pi
+        norm = data / np.sum(data)*2 * np.pi
+
+        # obtaining ordinates of bar edges
+        left = np.cumsum(np.append(0,
+                                                        norm.flatten()[:-1])).reshape(data.shape)
+
+        # Creating color scale
+        cmap = plt.get_cmap("tab20c")
+        outer_colors = cmap(np.arange(6)*4)
+        inner_colors = cmap(np.array([1, 2, 5, 6, 9,
+                                                                10, 12, 13, 15,
+                                                                17, 18, 20]))
+
+        # Creating plot
+        fig, ax = plt.subplots(figsize=(10, 7),
+                                                subplot_kw=dict(polar=True))
+
+        ax.bar(x=left[:, 0],
+                width=norm.sum(axis=1),
+                bottom=1-size,
+                height=size,
+                color=outer_colors,
+                edgecolor='w',
+                linewidth=1,
+                align="edge")
+
+        ax.bar(x=left.flatten(),
+                width=norm.flatten(),
+                bottom=1-2 * size,
+                height=size,
+                color=inner_colors,
+                edgecolor='w',
+                linewidth=1,
+                align="edge")
+
+        ax.set(title="Nested pie chart")
+        ax.set_axis_off()
+
+        # show plot
+        plt.show()
+
         
 def out ():
         print("out")
