001d0a25a7157fbf5e6894de83961e185c1c45cb
Prenses Metin <60544998+PrensesMTN@users.noreply.github.com>
2024-04-02 23:05:00 +0300

    Update GUI_tests.py

--- GUI_tests.py
+++ GUI_tests.py
@@ -8,6 +8,10 @@ import os
 from matplotlib import pyplot as plt
 import numpy as np
 
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+
 
 customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -33,7 +37,7 @@ class App(customtkinter.CTk):
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="PERSION", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
 
-        self.sidebar_Ekle = customtkinter.CTkButton(self.sidebar_frame, text="INCOME", command=self.income_tx)
+        self.sidebar_Ekle = customtkinter.CTkButton(self.sidebar_frame, text="INCOME", command=self.income)
         self.sidebar_Ekle.grid(row=2, column=0, padx=20, pady=10)
         self.sidebar_Düzenle = customtkinter.CTkButton(self.sidebar_frame,text="OUT", command=out)
         self.sidebar_Düzenle.grid(row=3, column=0, padx=20, pady=10)
@@ -68,64 +72,57 @@ class App(customtkinter.CTk):
           #create main entry and button
           self.entry_ara = customtkinter.CTkEntry(self, placeholder_text="Aramak istediğiniz ürünü giriniz.")
           self.entry_ara.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
-          income()
-
-          
  
         
-def income ():
-        print("income")
+    def income (self):
+                print("income")
 
-        # Creating dataset
-        size = 6
-        cars = ['AUDI', 'BMW', 'FORD',
-                        'TESLA', 'JAGUAR', 'MERCEDES']
+                # Creating dataset
+                size = 6
+                data = np.array([[23, 16], [17, 23],
+                                [35, 11], [29, 33],
+                                [12, 27], [41, 42]])
 
-        data = np.array([[23, 16], [17, 23],
-                                        [35, 11], [29, 33],
-                                        [12, 27], [41, 42]])
+                # normalizing data to 2 pi
+                norm = data / np.sum(data)*2 * np.pi
 
-        # normalizing data to 2 pi
-        norm = data / np.sum(data)*2 * np.pi
+                # obtaining ordinates of bar edges
+                left = np.cumsum(np.append(0,norm.flatten()[:-1])).reshape(data.shape)
 
-        # obtaining ordinates of bar edges
-        left = np.cumsum(np.append(0,
-                                                        norm.flatten()[:-1])).reshape(data.shape)
+                # Creating color scale
+                cmap = plt.get_cmap("tab20c")
+                outer_colors = cmap(np.arange(6)*4)
+                inner_colors = cmap(np.array([0, 2, 5, 6, 9,
+                                        10, 12, 13, 15,
+                                        17, 18, 20]))
 
-        # Creating color scale
-        cmap = plt.get_cmap("tab20c")
-        outer_colors = cmap(np.arange(6)*4)
-        inner_colors = cmap(np.array([1, 2, 5, 6, 9,
-                                                                10, 12, 13, 15,
-                                                                17, 18, 20]))
+                # Creating plot
+                fig, ax = plt.subplots(figsize=(10, 7),subplot_kw=dict(polar=True))
 
-        # Creating plot
-        fig, ax = plt.subplots(figsize=(10, 7),
-                                                subplot_kw=dict(polar=True))
+                ax.bar(x=left[:, 0],
+                        width=norm.sum(axis=1),
+                        bottom=1-size,
+                        height=size,
+                        color=outer_colors,
+                        edgecolor='w',
+                        linewidth=1,
+                        align="edge")
 
-        ax.bar(x=left[:, 0],
-                width=norm.sum(axis=1),
-                bottom=1-size,
-                height=size,
-                color=outer_colors,
-                edgecolor='w',
-                linewidth=1,
-                align="edge")
+                ax.bar(x=left.flatten(),
+                        width=norm.flatten(),
+                        bottom=1-2 * size,
+                        height=size,
+                        color=inner_colors,
+                        edgecolor='w',
+                        linewidth=1,
+                        align="edge")
 
-        ax.bar(x=left.flatten(),
-                width=norm.flatten(),
-                bottom=1-2 * size,
-                height=size,
-                color=inner_colors,
-                edgecolor='w',
-                linewidth=1,
-                align="edge")
+                ax.set(title="Income pie chart")
+                ax.set_axis_off()
 
-        ax.set(title="Nested pie chart")
-        ax.set_axis_off()
-
-        # show plot
-        plt.show()
+                # show plot
+                #plt.show()
+                
 
         
 def out ():
